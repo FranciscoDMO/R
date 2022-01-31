6af83ecb284e9c5d2746cbf45486d1b00ebb7390
Francisco Oliveira <franciscodmo19@gmail.com>
2022-01-31 17:49:34 +0000

    update

--- indexing _ data wraling _ plots/Assessment_Indexing.r
+++ indexing _ data wraling _ plots/Assessment_Indexing.r
@@ -15,3 +15,35 @@ ind<- murders$region == "Northeast" & low
 
 # Names of states in `ind` 
 murders$state[ind]
+
+# Compute the average murder rate using `mean` and store it in object named `avg`
+avg <-mean(murder_rate)
+
+# How many states have murder rates below avg ? Check using sum 
+sum(murder_rate<avg)
+
+
+# Store the 3 abbreviations in a vector called `abbs` (remember that they are character vectors and need quotes)
+abbs<- c("AK","MI","IA")
+# Match the abbs to the murders$abb and store in ind
+ind <-match(abbs, murders$abb)
+
+# Print state names from ind
+murders$state[ind]
+
+# Store the 5 abbreviations in `abbs`. (remember that they are character vectors)
+abbs<- c("MA", "ME", "MI", "MO", "MU")
+
+# Use the %in% command to check if the entries of abbs are abbreviations in the the murders data frame
+abbs %in% murders$abb
+
+
+
+# Store the 5 abbreviations in abbs. (remember that they are character vectors)
+abbs <- c("MA", "ME", "MI", "MO", "MU") 
+
+# Use the `which` command and `!` operator to find out which index abbreviations are not actually part of the dataset and store in `ind`
+ind <-which (!(abbs %in% murders$abb) )
+
+# Names of abbreviations in `ind`
+abbs[ind]
